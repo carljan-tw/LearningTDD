eb89a2b04db6e1a3cb2eec89fd86bf00da6d89a3
carljan-tw <b89502103@ntu.edu.tw>
2016-01-05 22:37:48 +0800

    ResetFolderAndTheFirstVersion
    Add the first test case

---
+++ day2-homework/PotterShoppingCart.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PotterShoppingCart  
+{
+    public class PotterBook
+    {
+        public String Name { get; set; }
+
+        public Decimal Price { get; set; }
+
+    }
+
+    public class PotterShoppingCart  
+    {
+        public Decimal CalculatePrice(IEnumerable<PotterBook> products)   
+        {
+            Decimal summaryPrice; 
+            var bookCount = products.Count(); 
+            summaryPrice = products.Sum(x => x.Price);
+            return summaryPrice;
+         }
+     }
+}
+
---
+++ day2-homework/PotterShoppingCartTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PotterShoppingCart;
+using System.Collections.Generic;
+
+namespace PotterShoppingCart.Tests
+{
+    [TestClass]
+    public class PotterShoppingCartTest
+    {
+        [TestMethod]
+        public void Buy_Vol1_1Book_Should_Be_100()
+        {
+            //arrange  
+            var cart = new PotterShoppingCart();
+            var products = new List<PotterBook>  
+            {
+                new PotterBook{Name = "Vol1", Price = 100}  
+            };
+
+            var expected = 100;
+
+            //act  
+            var actual = cart.CalculatePrice(products);
+
+            //assert  
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
