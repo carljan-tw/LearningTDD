8454d8be8a2f9f8298287275f4d00e926e6e6e17
carljan-tw <b89502103@ntu.edu.tw>
2016-01-05 23:05:43 +0800

    Pass the basic scenario
    Pass the basic scenario for the 5 cases

--- day2-homework/PotterShoppingCart.cs
+++ day2-homework/PotterShoppingCart.cs
@@ -17,9 +17,31 @@ namespace PotterShoppingCart
     {
         public Decimal CalculatePrice(IEnumerable<PotterBook> products)   
         {
-            Decimal summaryPrice; 
-            var bookCount = products.Count(); 
-            summaryPrice = products.Sum(x => x.Price);
+            Decimal summaryPrice, discount; 
+            var bookCount = products.Count();
+
+            if (bookCount == 2)
+            {
+                discount = 0.95m;
+            }
+            else if (bookCount == 3)
+            {
+                discount = 0.9m;
+            }
+            else if (bookCount == 4)
+            {
+                discount = 0.8m;
+            }
+            else if (bookCount == 5)
+            {
+                discount = 0.75m;
+            }
+            else
+            {
+                discount = 1;
+            }
+
+            summaryPrice = products.Sum(x => x.Price) * discount;
             return summaryPrice;
          }
      }
--- day2-homework/PotterShoppingCartTest.cs
+++ day2-homework/PotterShoppingCartTest.cs
@@ -26,5 +26,91 @@ namespace PotterShoppingCart.Tests
             //assert  
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void Buy_Vol1_1Book_Vol2_1Book_Should_Be_190()
+        {
+            //arrange  
+            var cart = new PotterShoppingCart();
+            var products = new List<PotterBook>  
+            {
+                new PotterBook{Name = "Vol1", Price = 100},
+                new PotterBook{Name = "Vol2", Price = 100}
+            };
+
+            var expected = 100 * 2 * 0.95m;
+
+            //act  
+            var actual = cart.CalculatePrice(products);
+
+            //assert  
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Buy_Vol1_1Book_Vol2_1Book_Vol3_1Book_Should_Be_270()
+        {
+            //arrange  
+            var cart = new PotterShoppingCart();
+            var products = new List<PotterBook>  
+            {
+                new PotterBook{Name = "Vol1", Price = 100},
+                new PotterBook{Name = "Vol2", Price = 100},
+                new PotterBook{Name = "Vol3", Price = 100}
+            };
+
+            var expected = 100 * 3 * 0.9m;
+
+            //act  
+            var actual = cart.CalculatePrice(products);
+
+            //assert  
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Buy_Vol1_1Book_Vol2_1Book_Vol3_1Book_Vol4_1Book_Should_Be_320()
+        {
+            //arrange  
+            var cart = new PotterShoppingCart();
+            var products = new List<PotterBook>  
+            {
+                new PotterBook{Name = "Vol1", Price = 100},
+                new PotterBook{Name = "Vol2", Price = 100},
+                new PotterBook{Name = "Vol3", Price = 100},
+                new PotterBook{Name = "Vol4", Price = 100},
+            };
+
+            var expected = 100 * 4 * 0.8m;
+
+            //act  
+            var actual = cart.CalculatePrice(products);
+
+            //assert  
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Buy_Vol1_1Book_Vol2_1Book_Vol3_1Book_Vol4_1Book_Vol5_1Book_Should_Be_375()
+        {
+            //arrange  
+            var cart = new PotterShoppingCart();
+            var products = new List<PotterBook>  
+            {
+                new PotterBook{Name = "Vol1", Price = 100},
+                new PotterBook{Name = "Vol2", Price = 100},
+                new PotterBook{Name = "Vol3", Price = 100},
+                new PotterBook{Name = "Vol4", Price = 100},
+                new PotterBook{Name = "Vol5", Price = 100}
+            };
+
+            var expected = 100 * 5 * 0.75m;
+
+            //act  
+            var actual = cart.CalculatePrice(products);
+
+            //assert  
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
